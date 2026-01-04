9ff655bc26447d3a9ed7dbbeefe276021c585f55
conseil-benjamin <benlenormand61@gmail.com>
2026-01-04 16:02:32 +0100

    refactor: clean projects by removing useless stuff on the screen

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,58 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import ProductDetails from 'components/ProductDetails';
 import Loader from 'components/Loader';
-import Home from 'components/Home';
-import Admin from 'components/Admin';
-import Checkout from 'components/Checkout';
+import { GithubCorner, GithubStarButton } from 'components/Github';
+import Filter from 'components/Filter';
+import Products from 'components/Products';
+import Cart from 'components/Cart';
+import SearchBar from 'components/SearchBar';
 
 import { useProducts } from 'contexts/products-context';
 import * as S from './style';
+import Admin from 'components/Admin';
+import Checkout from 'components/Checkout';
 
 function App() {
-  const { isFetching } = useProducts();
+  const { isFetching, products, fetchProducts } = useProducts();
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
 
   return (
-    <BrowserRouter>
-      <S.Container>
-        {isFetching && <Loader />}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
-      </S.Container>
-    </BrowserRouter>
+    <S.Container>
+      {isFetching && <Loader />}
+      <GithubCorner />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <S.TwoColumnGrid>
+              <S.Side>
+                <Filter />
+                <GithubStarButton />
+              </S.Side>
+              <S.Main>
+                <S.MainHeader>
+                  <SearchBar />
+
+                </S.MainHeader>
+                <Products products={products} />
+              </S.Main>
+            </S.TwoColumnGrid>
+          }
+        />
+
+        {/* Route pour le détail du produit */}
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+
+      <Cart />
+    </S.Container>
   );
 }
 
